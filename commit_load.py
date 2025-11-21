54c0b4bbc238ca13e974b976a3e667be1a43ab9f
Carissa Chen <jac7az@gmail.com>
2025-11-21 14:11:39 -0500

    finish data loading

--- commit_load.py
+++ commit_load.py
@@ -4,9 +4,8 @@ import sys
 import duckdb
 import logging
 
-GITHUB_TOKEN=os.getenv("")
-print(GITHUB_TOKEN)
-GITHUB_AUTH_HEADER={"Authorization": "token".format(GITHUB_TOKEN)} if GITHUB_TOKEN else {}
+GITHUB_TOKEN=os.getenv("GITHUB_TOKEN")
+GITHUB_AUTH_HEADER={"Authorization": f"token {GITHUB_TOKEN}" if GITHUB_TOKEN else {}}
 REPOS = [
     ('pandas-dev', 'pandas'),
     ('numpy', 'numpy'),
@@ -40,25 +39,29 @@ def get_commits(url):   #enter api python package url here
             print(f"An error occurred: {e}")
             break
 
-def process_commits(package, commit):
-    con=duckdb.connect(database='packages.duckdb',read_only=False)
-    logger.info("Connected to DuckDB database.")
-
+def create_table():
     con.execute("""CREATE OR REPLACE TABLE commits (package VARCHAR, date TIMESTAMPTZ, author VARCHAR);""")
+    logger.info("Created commits table in DuckDB database.")
+
+def process_commits(package, commit):
     if commit['author']['name'] != 'dependabot[bot]':
-        con.execute("""INSERT INTO commits VALUES (?,?, ?);""", (package,commit['author']['date'], commit['author']['name']))
+        con.execute(f"""INSERT INTO commits VALUES (?,?, ?);""", (package,commit['author']['date'], commit['author']['name']))
         logger.info(f"Inserted commit data into commits table: {commit['author']['name']}")
 
 if __name__ == "__main__":
     try:
+        con=duckdb.connect(database='packages.duckdb',read_only=False)
+        logger.info("Connected to DuckDB database.")
+        create_table()
+
         for package in REPOS:
             for page in get_commits(f"https://api.github.com/repos/{package[0]}/{package[1]}/commits"):  
                 commits=page.json()
                 for c in commits:
                     process_commits(package[1],c['commit'])
-                    print(c['commit']['author']['name'],'\n')
-                    print(c['commit']['message'])
-      
+        #             print(c['commit']['author']['name'],'\n')
+        #             print(c['commit']['message'])
+
     except KeyboardInterrupt:
         quota_url="https://api.github.com/rate_limit"
         session=requests.Session()
